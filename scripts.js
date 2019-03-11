222ede64c2f5cb277bb64510554852f8b9043582
RGB Studios <justingolden21@gmail.com>
2019-03-11 02:03:58 -0700

    many updates
    implemented working board generator from sudoku.js
    
    commented old code (will be removed shortly)

--- scripts.js
+++ scripts.js
@@ -1,20 +1,11 @@
 $(function() {
 	// make the board
-	for(let x=0; x<9; x++) {
-		$('#sudoku-div').append('<div class="input-row">');
-		for(y=0; y<9; y++) {
-			let cssClass = '';
-			if(x==2 || x==5) cssClass += ' border-bottom';
-			if(y==2 || y==5) cssClass += ' border-right';
-			$('#sudoku-div').append('<input id="s' + x + '-' + y + '" class="' + cssClass + '" type="number" min="1" max="9" required>');
-		}
-		$('#sudoku-div').append('</div>');
-	}
+	makeBoard();
 
 	// set the board
-	reset();
+	// reset();
 
-	$('#s0-2').select();
+	// $('#s0-2').select();
 
 	$('input').change(function() {
 		checkValids();
@@ -25,55 +16,89 @@ $(function() {
 		}
 	});
 
-	$('#solution-btn').click(function() {
-		setSolution();
-		$('input').addClass('won');
-	});
+	// $('#solution-btn').click(function() {
+	// 	setSolution();
+	// 	$('input').addClass('won');
+	// });
 
 	// $('#reset-btn').click(function() {
 	// 	reset();
 	// });
 
+	$('#easy-btn').click(function() {
+		setDifficulty('easy');
+	});
+	$('#medium-btn').click(function() {
+		setDifficulty('medium');
+	});
+	$('#hard-btn').click(function() {
+		setDifficulty('hard');
+	});
+
 });
 
-
-function setSolution() {
-	setBoard([
-		'534678912'.split(''),
-		'672195348'.split(''),
-		'198342567'.split(''),
-		'859761423'.split(''),
-		'426853791'.split(''),
-		'713924856'.split(''),
-		'961537284'.split(''),
-		'287419635'.split(''),
-		'345286179'.split('')
-	], false);	
+function setDifficulty(difficulty) {
+	setBoard(sudoku.board_string_to_grid(sudoku.generate(difficulty) ) );
+	$('	input').removeClass('invalid');
 }
 
-function reset() {
-	setBoard([
-		['5','3',' ',' ','7',' ',' ',' ',' '],
-		['6',' ',' ','1','9','5',' ',' ',' '],
-		[' ','9','8',' ',' ',' ',' ','6',' '],
-		['8',' ',' ',' ','6',' ',' ',' ','3'],
-		['4',' ',' ','8',' ','3',' ',' ','1'],
-		['7',' ',' ',' ','2',' ',' ',' ','6'],
-		[' ','6',' ',' ',' ',' ','2','8',' '],
-		[' ',' ',' ','4','1','9',' ',' ','5'],
-		[' ',' ',' ',' ','8',' ',' ','7','9']
-	], true);
-	// todo: make it work, checking for blanks in set board makes it ignore blanks, but we don't want ti disable blanks either
+
+function makeBoard() {
+	for(let x=0; x<9; x++) {
+		$('#sudoku-div').append('<div class="input-row">');
+		for(y=0; y<9; y++) {
+			let cssClass = '';
+			if(x==2 || x==5) cssClass += ' border-bottom';
+			if(y==2 || y==5) cssClass += ' border-right';
+			$('#sudoku-div').append('<input id="s' + x + '-' + y + '" class="' + cssClass + '" type="number" min="1" max="9" required>');
+		}
+		$('#sudoku-div').append('</div>');
+	}	
 }
 
-function setBoard(vals, setDisabled) {
+
+// function setSolution() {
+// 	setBoard([
+// 		'534678912'.split(''),
+// 		'672195348'.split(''),
+// 		'198342567'.split(''),
+// 		'859761423'.split(''),
+// 		'426853791'.split(''),
+// 		'713924856'.split(''),
+// 		'961537284'.split(''),
+// 		'287419635'.split(''),
+// 		'345286179'.split('')
+// 	], false);	
+// }
+
+// function reset() {
+// 	setBoard([
+// 		['5','3',' ',' ','7',' ',' ',' ',' '],
+// 		['6',' ',' ','1','9','5',' ',' ',' '],
+// 		[' ','9','8',' ',' ',' ',' ','6',' '],
+// 		['8',' ',' ',' ','6',' ',' ',' ','3'],
+// 		['4',' ',' ','8',' ','3',' ',' ','1'],
+// 		['7',' ',' ',' ','2',' ',' ',' ','6'],
+// 		[' ','6',' ',' ',' ',' ','2','8',' '],
+// 		[' ',' ',' ','4','1','9',' ',' ','5'],
+// 		[' ',' ',' ',' ','8',' ',' ','7','9']
+// 	], true);
+// 	// todo: make it work, checking for blanks in set board makes it ignore blanks, but we don't want to disable blanks either
+// }
+
+function setBoard(vals, setDisabled=true) {
 	for(let x=0; x<9; x++) {
 		for(y=0; y<9; y++) {
-			if(vals[x][y] != ' ') {
+			if(vals[x][y] != '.') {
 				$('#s' + x + '-' + y).val(vals[x][y]);
 				if(setDisabled) {
 					$('#s' + x + '-' + y).prop('disabled', 'true');
 				}
+			} else {
+				$('#s' + x + '-' + y).val('');
+				if(setDisabled) {
+					$('#s' + x + '-' + y).prop('disabled', '');
+				}	
 			}
 		}
 	}
